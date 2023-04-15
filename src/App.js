145a78aaf2b9b6489dde860ecf0d6bf6bb808d8c
Oussama_LD <oussama.labiod@inttic.dz>
2023-04-15 14:27:38 +0100

    init react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import GoogleLogin from 'react-google-login';
+import { gapi } from "gapi-script";
+import { useEffect } from 'react';
 function App() {
+  useEffect(() => {
+    gapi.load("auth2", () => {
+      gapi.auth2.init({ clientId:'680157492797-7l7kif0cuvflg6m3u2kno4a24n776b11.apps.googleusercontent.com' });
+    });
+  }, []);
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoogleLogin
+        clientId="680157492797-7l7kif0cuvflg6m3u2kno4a24n776b11.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />,
     </div>
   );
 }
